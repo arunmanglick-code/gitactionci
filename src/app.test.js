5a0f0dcfaf4707ac1e6a0d7f9adf7d75001ec2d8
arunmanglick-code <arunmanglick@gmail.com>
2022-06-18 02:31:32 -0400

    Update app.test.js

--- src/app.test.js
+++ src/app.test.js
@@ -2,5 +2,5 @@ const { dayOfTheWeek } = require('./app');
 
 test('getDay returns the long-format day of the week', () => {
     const day = dayOfTheWeek( new Date('6/18/2022') );
-    expect( day ).toBe('Saturday');
+    expect( day ).toBe('Wednesday');
 });
